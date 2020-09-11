39fefc372f8b3f4be2b5ad790a02e87c90491206
JesusPineda <pinedajesus79@gmail.com>
2020-09-11 16:20:43 -0600

    Se agregaron try catch para validar errores

--- routers/api/products.js
+++ routers/api/products.js
@@ -1,33 +1,44 @@
-
 const router = require('express').Router();
-const {Producto}  =  require('../../dbconfig')
-
+const {Producto}  =  require('../../dbconfig');
 //Se crean las rutas para una API REST con los diferente metodos 
 
-router.get('/', async (req,res)=>{ // 
-    let producto = await Producto.findAll();
-    res.json(producto);
+router.get('/', async (req,res)=>{ 
+    console.log(req.usuarioId);
+    let productos = await Producto.findAll();
+    res.json(productos);
 });
-
+router.get('/productbycategory', async (req,res)=>{ 
+    let productbycategory = await Producto.findAll({
+        include:'categorias'
+    });
+    res.json(productbycategory);
+});
 router.post('/', async (req,res)=>{
-    let producto = await Producto.create(req.body);
-    res.json(producto);
+    try {
+        let producto = await Producto.create(req.body);
+        res.json(producto);
+    } catch (error) {
+        res.json(error);
+    }
 });
-
 router.put('/:productId', async (req,res)=>{ //estas rutas reciben parametros 
-    await Producto.update(req.body,{ // funcion para actualizar 
-        where: { idProducto: req.params.productId }
-    });
-    res.json({success: 'Se ha actualizado un registro.'});
-    
+    try {
+        await Producto.update(req.body,{ // funcion para actualizar 
+            where: { productoId: req.params.productId }
+        });
+        res.json({success: 'Se ha actualizado un registro.'});
+    } catch (error) {
+        res.json(error);
+    }
 });
-
 router.delete('/:productId', async (req,res)=>{ //estas rutas reciben parametros 
-    await Producto.destroy ({ // funcion para borrar 
-        where: { idProducto: req.params.productId }
-    });
-    res.json({success: 'Se ha borrado un registro.'});
-    
+    try {
+        await Producto.destroy ({ // funcion para borrar 
+            where: { productoId: req.params.productId }
+        });
+        res.json({success: 'Se ha borrado un registro.'});
+    } catch (error) {
+        res.json(error)
+    } 
 });
-
 module.exports = router; // se exporta el router hacia api.js
