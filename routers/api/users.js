d885d479ed8d5161b0d080d43a95269397bea0fd
JesusPineda <pinedajesus79@gmail.com>
2020-09-11 16:22:06 -0600

    Se agrego una funcion de inicio de sesion

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,15 +1,58 @@
-
 const router = require('express').Router();
-const {Usuarios}  =  require('../../dbconfig')
+const {Usuarios}  =  require('../../dbconfig');
+const bcrypt  =  require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const token = require('./createToken');
 
 //Se crean las rutas para una API REST con los diferente metodos 
+
+//Funcion para validar el incio de sesion
+
+router.post('/login', [
+    // Se validan los datos 
+    check('usuario','El nombre del usario es obligatorio').not().isEmpty(),
+    check('pass','La contraseña es obligatoria').not().isEmpty(), 
+],async (req,res)=>{ 
+    // Funcion para validar datos 
+    const erros = validationResult(req); 
+    //Si existe un error devuelve el codigo 422
+    if(!erros.isEmpty()){      
+        return res.status(422).json({errores: erros.array()});
+    }
+    //Se busca el usuario en la base de datos 
+    const user = await Usuarios.findOne({ where: {usuario: req.body.usuario } });
+    
+    if(user){
+        //Si el usuario existe compara las contraseñas encriptadas
+        const iguals = bcrypt.compareSync(req.body.pass, user.pass);
+        if(iguals){
+            //Si la cotraseña es correcta se crea un token de sesion
+            res.json({success: token.createToken(user)});
+            
+        }else{
+            res.json({error:'Error en usuario o contraseña!'});
+        }
+    }else{
+        res.json({error:'Error en usuario o contraseña!'});
+    }
+});
 
 router.get('/', async (req,res)=>{ // 
     let usuarios = await Usuarios.findAll();
     res.json(usuarios);
 });
 
-router.post('/', async (req,res)=>{
+router.post('/',[
+    check('usuario','El nombre del usario es obligatorio').not().isEmpty(),
+    check('pass','La contraseña es obligatoria').not().isEmpty(),
+], async (req,res)=>{
+
+    const erros = validationResult(req);
+
+    if(!erros.isEmpty()){
+        return res.status(422).json({errores: erros.array()});
+    }
+    req.body.pass = bcrypt.hashSync(req.body.pass, 10);
     let usuarios = await Usuarios.create(req.body);
     res.json(usuarios);
 });
